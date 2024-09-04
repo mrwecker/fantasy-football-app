29eb1a1f09ac1b6dbfb55b446ebc52a9039c4c87
Michael Wecker <mrwecker@gmail.com>
2024-09-04 19:37:03 +0200

    Initial commit

---
+++ ETL_Football_pipeline.py
@@ -0,0 +1,198 @@
+# THIS IS JUST THE PIPELINE TO GET CSV - USE fantasy_app.py TO LAUNCH APP
+import pandas as pd
+import requests
+import json
+from datetime import datetime
+import numpy as np
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename='/Users/michaelwecker/Hyper_Island/Final_Project/logs/etl_pipeline.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def fetch_data():
+    logging.info('Fetching data from API...')
+    try:
+        url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status
+        data = json.loads(response.content)
+        
+        players_df = pd.DataFrame(data['elements'])
+        teams_df = pd.DataFrame(data['teams'])
+        events_df = pd.DataFrame(data['events'])
+        events_df['gameweek_date'] = pd.to_datetime(events_df['deadline_time']).dt.date
+        events_df = events_df.drop(columns=['deadline_time'])
+        
+        # Determine the current gameweek based on today's date
+        today = datetime.now().date()
+        current_gameweek = events_df[events_df['gameweek_date'] >= today].iloc[0]['id']
+        
+        logging.info(f'Data fetched successfully. Current gameweek: {current_gameweek}')
+        return players_df, teams_df, events_df, current_gameweek
+    except Exception as e:
+        logging.error(f'Error fetching data: {e}')
+        raise
+
+def merge_player_team_data(players_df, teams_df):
+    logging.info('Merging player and team data...')
+    try:
+        merged_df = pd.merge(left=players_df, right=teams_df, how='left', left_on='team', right_on='id', suffixes=('_player', '_team'))
+        logging.info('Merge complete')
+        return merged_df
+    except Exception as e:
+        logging.error(f'Error merging data: {e}')
+        raise
+
+def select_columns(df):
+    logging.info('Selecting columns...')
+    try:
+        selected_column_names = [
+            'id_player', 'first_name', 'second_name', 'element_type', 'now_cost', 'name', 'total_points', 
+            'goals_scored', 'assists', 'clean_sheets', 'points_per_game_rank', 'value_form', 'value_season', 
+            'ict_index', 'expected_goals', 'expected_assists', 'expected_goal_involvements', 'influence_rank', 
+            'form_rank', 'form_player', 'in_dreamteam', 'news', 'chance_of_playing_next_round', 'points_per_game', 
+            'ict_index_rank', 'expected_goals_per_90', 'expected_assists_per_90', 'strength_overall_home', 
+            'strength_overall_away', 'team', 'team_code'
+        ]
+        selected_df = df[selected_column_names]
+        logging.info('Column selection complete')
+        return selected_df
+    except KeyError as e:
+        logging.error(f'KeyError selecting columns: {e}')
+        raise
+    except Exception as e:
+        logging.error(f'Error selecting columns: {e}')
+        raise
+
+def get_player_stats(player_id):
+    logging.info(f'Fetching player stats for player ID: {player_id}')
+    try:
+        url = f'https://fantasy.premierleague.com/api/element-summary/{player_id}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json().get('fixtures', [])
+        fixtures_data = [
+            {
+                'event_name': fixture.get('event_name', None),
+                'difficulty': fixture.get('difficulty', None),
+            }
+            for fixture in data
+        ]
+        player_stats_df = pd.DataFrame({
+            'PlayerID': [player_id] * len(fixtures_data),
+            'EventName': [fixture['event_name'] for fixture in fixtures_data],
+            'Difficulty': [fixture['difficulty'] for fixture in fixtures_data],
+        })
+        logging.info(f'Player stats fetched for player ID: {player_id}')
+        return player_stats_df
+    except Exception as e:
+        logging.error(f'Error fetching player stats for player ID {player_id}: {e}')
+        raise
+
+def compile_difficulty_data(player_ids):
+    logging.info('Compiling difficulty data for all players...')
+    try:
+        all_player_data = []
+        for player_id in player_ids:
+            player_stats = get_player_stats(player_id)
+            if player_stats is not None:
+                all_player_data.append(player_stats)
+        difficulty_df = pd.concat(all_player_data, ignore_index=True)
+        logging.info('Difficulty data compilation complete')
+        return difficulty_df
+    except Exception as e:
+        logging.error(f'Error compiling difficulty data: {e}')
+        raise
+
+def process_data(players_df, difficulty_df, current_gameweek):
+    logging.info('Processing data...')
+    try:
+        pivot_df = difficulty_df.pivot_table(index='PlayerID', columns='EventName', values='Difficulty')
+        pivot_df_sorted = pivot_df.sort_values(by='PlayerID', ascending=True)
+        pivot_df_sorted.reset_index(inplace=True)
+
+        full_df = pd.merge(left=players_df, right=pivot_df_sorted, how='left', left_on='id_player', right_on='PlayerID')
+        
+        element_mapping = {1: 'GK', 2: 'DEF', 3: 'MID', 4: 'FWD'}
+        full_df["element_type"] = full_df["element_type"].replace(element_mapping)
+        
+        full_df.rename(columns={
+            'id_player': 'Player ID', 
+            'first_name': 'First Name', 
+            'second_name': 'Last Name',
+            'element_type': 'Position',
+            'now_cost': 'Cost',
+            'name': 'Team',
+            'total_points': 'Total points',
+            'goals_scored': 'Goals scored',
+            'assists': 'Assists',
+            'clean_sheets': 'Clean sheets',
+            'points_per_game_rank': 'Points per game rank',
+            'value_form': 'Value form',
+            'value_season': 'Value season',
+            'ict_index': 'ICT Index',
+            'expected_goals': 'Expected goals',
+            'expected_assists': 'Expected assists',
+            'expected_goal_involvements': 'Expected goal involvements',
+            'influence_rank': 'Influence rank',
+            'form_rank': 'Form rank',
+            'form_player': 'Player form',
+            'in_dreamteam': 'In dream team',
+            'news': 'News',
+            'chance_of_playing_next_round': 'Chance of playing next round',
+            'points_per_game': 'Points per game',
+            'ict_index_rank': 'ICT Index rank',
+            'expected_goals_per_90': 'Expected goals per 90',
+            'expected_assists_per_90': 'Expected assists per 90',
+            'strength_overall_home': 'Strength overall home',
+            'strength_overall_away': 'Strength overall away'
+        }, inplace=True)
+        
+        full_df["Player form"] = pd.to_numeric(full_df["Player form"], errors='coerce').fillna(0)
+        full_df["Player form"] = full_df["Player form"].clip(lower=0)
+        
+        gameweek_columns = [col for col in full_df.columns if col.startswith('Gameweek')]
+        sorted_gameweek_columns = sorted(gameweek_columns, key=lambda x: int(x.split()[1]))
+        other_columns = [col for col in full_df.columns if not col.startswith('Gameweek')]
+        full_df = full_df[other_columns + sorted_gameweek_columns]
+        
+        gameweek_start_column = full_df.columns.get_loc(f'Gameweek {current_gameweek}')
+        
+        def sum_next_5_non_nan(row):
+            non_nan_values = row.iloc[gameweek_start_column:].dropna().head(5)
+            return np.sum(non_nan_values)
+
+        full_df["Difficulty_score"] = full_df.apply(sum_next_5_non_nan, axis=1).round(3)
+        full_df["Fixture Difficulty Index"] = (full_df["Player form"] / full_df["Difficulty_score"]).round(3)
+        full_df['full_name'] = full_df['First Name'] + ' ' + full_df['Last Name']
+        
+        logging.info('Data processing complete')
+        return full_df
+    except Exception as e:
+        logging.error(f'Error processing data: {e}')
+        raise
+
+def main():
+    logging.info('Starting ETL pipeline')
+    try:
+        players_df, teams_df, events_df, current_gameweek = fetch_data()
+        player_teams_data = merge_player_team_data(players_df, teams_df)
+        selected_df = select_columns(player_teams_data)
+        player_ids = players_df["id"]
+        difficulty_df = compile_difficulty_data(player_ids)
+        processed_df = process_data(selected_df, difficulty_df, current_gameweek)
+        
+        processed_df.to_csv("data_processed_player.csv", index=False)
+        events_df.to_csv("data_events.csv", index=False)
+        players_df.to_csv("data_players.csv", index=False)
+        teams_df.to_csv("data_teams.csv", index=False)
+        logging.info('ETL pipeline completed successfully')
+    except Exception as e:
+        logging.error(f'ETL pipeline failed: {e}')
+
+if __name__ == "__main__":
+    main()
---
+++ final_fantasy_app_1.py
@@ -0,0 +1,171 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+import os
+
+# Load the data using st.cache_data to improve performance
+@st.cache_data
+def load_data():
+    base_path = "/Users/michaelwecker/Hyper_Island/Final_Project"
+    file_path = os.path.join(base_path, "data_processed_player.csv")
+    
+    # Debug: Check if the file path is correct and if the file exists
+    if not os.path.exists(file_path):
+        st.error(f"File not found: {file_path}")
+        return pd.DataFrame()  # Return an empty DataFrame if file not found
+
+    return pd.read_csv(file_path)
+
+# Load the processed data
+processed_df = load_data()
+
+# If the data is empty, stop further execution
+if processed_df.empty:
+    st.stop()
+
+# Display the introductory text
+st.markdown("""
+    # Welcome to the Fantasy Football Player Selector
+    
+    This tool will enable you to make calculated decisions on which player to purchase based on a metric calculated from historical statistics. 
+
+    Players' current form is weighted against an average difficulty of the upcoming 5 matches to predict how many points they could make which determines the 'Fixture Difficulty Index'. The higher the number, the better the higher likelihood that a player is in good form, playing against easier teams, and is expected to make more points. This can be viewed easily by the size of the circle. The bigger the better!
+
+    Firstly, use the filters on the left of the page to filter by: 
+    1. Position
+    2. Cost
+    3. Team
+
+    You can have multiple positions and teams, but to compare players, it is recommended to view them by individual positions due to how the players' form is calculated. 
+    Players with high form and easy fixtures will be located closer to the top left. Players with poor form and difficult fixtures will be located closer to the bottom right. Once you have located the best few players (near the top left and larger circles), you can hover over them to see their name, team, and price. You can see further statistics in the table below. 
+
+    Good luck!
+""")
+
+# Sidebar filters
+st.sidebar.header("Filters")
+
+# Position filter
+positions = st.sidebar.multiselect(
+    'Select Position(s)',
+    options=processed_df['Position'].unique(),
+    default=processed_df['Position'].unique()
+)
+
+# Cost range filter
+min_cost = processed_df['Cost'].min()
+max_cost = processed_df['Cost'].max()
+cost_range = st.sidebar.slider(
+    'Select Player Cost Range',
+    min_value=int(min_cost),
+    max_value=int(max_cost),
+    value=(int(min_cost), int(max_cost))
+)
+
+# Team filter
+teams = st.sidebar.multiselect(
+    'Select Team(s)',
+    options=processed_df['Team'].unique(),
+    default=processed_df['Team'].unique()
+)
+
+# Apply filters
+filtered_df = processed_df[
+    (processed_df['Position'].isin(positions)) &
+    (processed_df['Cost'].between(*cost_range)) &
+    (processed_df['Team'].isin(teams))
+]
+
+# Generate the scatter plot with px
+fig_px = px.scatter(
+    filtered_df,
+    x='Difficulty_score',
+    y='Player form',
+    color='Fixture Difficulty Index',
+    size='Fixture Difficulty Index',
+    labels={
+        'Player form': 'Current Form/Expected Points',
+        'Difficulty_score': 'Difficulty Average for Upcoming 5 Games'
+    },
+    hover_data={'full_name': True, 'Fixture Difficulty Index': True, 'Team': True, 'Cost': True},
+    title='Current Form vs Difficulty of Upcoming Games for Midfielders',
+    size_max=30,
+    color_continuous_scale=[
+        (0.0, 'red'),
+        (0.2, 'orange'),
+        (0.4, 'yellow'),
+        (0.9, 'green'),
+        (1.0, 'darkgreen')
+    ]
+)
+
+# Define gradient background
+x = np.linspace(0, 20, 100)  # Adjust x-axis range as needed
+y = np.linspace(-2, 15, 100) # Adjust y-axis range as needed
+X, Y = np.meshgrid(x, y)
+
+# Calculate the gradient (increase diagonal effect)
+Z = np.sqrt(((X - 20) / 30) ** 2 + ((Y - 13) / 7.5) ** 2)  # Shift the diagonal
+Z = (Z - np.min(Z)) / (np.max(Z) - np.min(Z))  # Normalize to [0, 1]
+
+# Create heatmap with diagonal gradient
+fig_bg = go.Figure()
+fig_bg.add_trace(
+    go.Heatmap(
+        z=Z,
+        x=x,
+        y=y,
+        colorscale=[[0, 'green'], [1, 'red']],  # Green to red transition
+        showscale=False,  # Hide the color scale bar
+        zmin=0,
+        zmax=1,
+        opacity=0.6  # Adjust opacity if needed
+    )
+)
+
+# Overlay the scatter plot on top of the gradient background
+for trace in fig_px.data:
+    fig_bg.add_trace(trace)
+
+# Add color scale title using annotations
+fig_bg.add_annotation(
+    x=1.35,  # Position further to the right of the plot
+    y=1.05,  # Position at the top of the plot
+    text="Fixture Difficulty Index",
+    showarrow=False,
+    xref="paper",
+    yref="paper",
+    font=dict(size=14)
+)
+
+# Update layout to ensure heatmap covers entire chart
+fig_bg.update_layout(
+    xaxis=dict(
+        title='Difficulty Average for Upcoming 5 Games',  # X-axis title
+        range=[0, 21]  # Match x-axis range to heatmap
+    ),
+    yaxis=dict(
+        title='Current Form/Expected Points',  # Y-axis title
+        range=[-2, 15]  # Match y-axis range to heatmap
+    ),
+    showlegend=False,
+    height=800,
+    #title=dict(
+        #text='Current Form vs Difficulty of Upcoming Games for Midfielders',
+        #x=0.5,  # Center the title
+        #xanchor='center'
+    #),
+    margin=dict(r=200)  # Increase the right margin to accommodate the annotation
+)
+
+# Set axis ranges to focus on relevant data
+fig_bg.update_xaxes(range=[9, 20.1])
+fig_bg.update_yaxes(range=[0, 15])
+
+# Display the plot
+st.plotly_chart(fig_bg)
+
+# Display the filtered data
+st.dataframe(filtered_df)
